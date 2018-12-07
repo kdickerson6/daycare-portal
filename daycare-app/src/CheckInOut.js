8c7625f430e5c80b905f912c0ecc10ac1c1834d4
Stephanie Aloy Cruz <stephanie.a.cruz@aexp.com>
2018-12-07 17:07:50 -0500

    filling in data

--- daycare-app/src/CheckInOut.js
+++ daycare-app/src/CheckInOut.js
@@ -3,7 +3,7 @@ import { Button } from 'react-materialize';
 import Confirmation from './Confirmation.js';
 
 let peopleData = require('./testData.json');
-let testPerson = peopleData.persons[1];
+let testPerson = peopleData.daycare.member;
 
 class CheckInOut extends Component {
     constructor(props) {
@@ -16,9 +16,9 @@ class CheckInOut extends Component {
     }
 
     getLastAction() {
-        if(testPerson.last_action.type === 'check_in') {
+        if(testPerson.latest_check.type === 'in') {
             this.setState({checkIn: true});
-        } else if(testPerson.last_action.type === 'check_out') {
+        } else if(testPerson.latest_check.type === 'out') {
             this.setState({checkIn: false});
         }
     }
@@ -29,19 +29,23 @@ class CheckInOut extends Component {
 
     checkInOutHandler() {
         // For now console logging -- will obviously store something in the DB
+        var currentTimestamp = new Date().toUTCString();
+
         if(this.state.checkIn) {
-            console.log('Check IN Recorded');
-            this.setState({action: 'checked in'});
-        } else {
             console.log('Check OUT Recorded');
+            this.setState({action: 'checked in'});
+            console.log('Checked OUT at ' + currentTimestamp);
+        } else {
+            console.log('Check IN Recorded');
             this.setState({action: 'checked out'});
+            console.log('Checked IN at ' + currentTimestamp);
         }
 
         this.setState({confirmed: true});
     }
 
     render() {
-        this.getLastAction.bind(this);
+        this.getLastAction();
         const isCheckIn = this.state.checkIn;
         const isConfirmed = this.state.confirmed;
 
@@ -52,9 +56,9 @@ class CheckInOut extends Component {
                 :
                 <div className='PersonInfo'>
                     <h4>ID NUMBER: {this.props.currentId}</h4>
-                    <h4>Name: {testPerson.name}</h4>
-                    {isCheckIn ? <h5>Last Check Out Time: {testPerson.last_action.time}</h5> : <h5>Last Check In Time: {testPerson.last_action.time}</h5>}
-                    <Button className='red lighten-2' waves='light' onClick={this.cancelClickHandler.bind(this)}>CANCEL</Button> {isCheckIn ? <Button waves='light' onClick={this.checkInOutHandler.bind(this)}>CHECKIN</Button> : <Button waves='light' onClick={this.checkInOutHandler.bind(this)}>CHECKOUT</Button>}
+                    <h4>Name: {testPerson.first_name + ' ' + testPerson.last_name}</h4>
+                    {isCheckIn ? <h5>Last Check In Time: {testPerson.latest_check.time}</h5> : <h5>Last Check Out Time: {testPerson.latest_check.time}</h5>}
+                    <Button className='red lighten-2' waves='light' onClick={this.cancelClickHandler.bind(this)}>CANCEL</Button> {isCheckIn ? <Button waves='light' onClick={this.checkInOutHandler.bind(this)}>CHECKOUT</Button> : <Button waves='light' onClick={this.checkInOutHandler.bind(this)}>CHECKIN</Button>}
                 </div>}       
             </div>
         );
--- daycare-app/src/Graphs.js
+++ daycare-app/src/Graphs.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Row, Col, Icon } from 'react-materialize';
+import { LineChart } from 'react-chartkick';
+import data from './chart_data.json';
 
 class Graphs extends Component {
     render() {
         return(
             <div>
-                <h4>Teacher graphs and stuff </h4>
+                <h4>Check In and Check Out Trends</h4>
                 <Row>
-                    <Col offset="s2 m2 l3" s={6} m={4} l={4}><Icon large>insert_chart</Icon></Col>
-                    <Col s={6} m={4} l={4}><Icon large>insert_chart</Icon></Col>
+                    <Col offset="s0 m1 l1" s={6} m={8} l={8}><LineChart data={data}/></Col>
                 </Row>
             </div>
         );
